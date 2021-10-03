23165d817f976d4c9c6a6ec9931a753c74759058
Den <denvanrain@gmail.com>
2021-10-03 20:19:42 +0300

    Add validations to Package of tracking_number

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,3 +1,5 @@
 class Package < ApplicationRecord
   enum delivery_status: { new: 'new', processing: 'processing', delivered: 'delivered', cancelled: 'cancelled' }, _prefix: :status
+
+  validates_uniqueness_of :tracking_number
 end
