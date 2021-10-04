a47f82eb483acac8216cabbf23dfd43cd218fb5c
Den <denvanrain@gmail.com>
2021-10-04 11:17:44 +0300

    Add validation to Package
    The lenth of tracking number

--- app/models/package.rb
+++ app/models/package.rb
@@ -2,4 +2,5 @@ class Package < ApplicationRecord
   enum delivery_status: { new: 'new', processing: 'processing', delivered: 'delivered', cancelled: 'cancelled' }, _prefix: :status
 
   validates_uniqueness_of :tracking_number
+  validates_length_of :tracking_number, is: 12
 end
